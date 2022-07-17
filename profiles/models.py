f92f33546fecb366cdf518c25f1856390360c750
jmjry <jakeryan1998@icloud.com>
2022-07-17 18:56:01 +0000

    Started relationships model so users can add friends on the site

--- profiles/models.py
+++ profiles/models.py
@@ -38,3 +38,14 @@ class Profile(models.Model):
                 to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
+
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+        
