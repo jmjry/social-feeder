71462ae2db26b7973c5ecafe464cc496e48695ee
jmjry <jakeryan1998@icloud.com>
2022-07-17 16:35:42 +0000

    Added Icons CSS from Feather Icons

--- socialfeed/settings.py
+++ socialfeed/settings.py
@@ -133,6 +133,8 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
  
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
