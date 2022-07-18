224557d6f7c74f506661e38f8448d31d6b7095fa
jmjry <jakeryan1998@icloud.com>
2022-07-18 20:09:38 +0000

    Created user invites template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import ( 
-    my_profile_view, 
+    my_profile_view,
+    invites_received_view,
 )
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('myprofile/', my_profile_view, name='my-profile-view'),
+    path('my-invites/', invites_received_view, name='my-invites-view'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Relationship
 from .forms import ProfileModelForm
 # Create your views here.
 
@@ -21,3 +21,19 @@ def my_profile_view(request):
     }
 
     return render(request, 'profiles/myprofile.html', context)
+
+
+def invites_received_view(request):
+    profile = Profile.objects.get(user=request.user)
+    qs = Relationship.objects.invatations_received(profile)
+    results = list(map(lambda x: x.sender, qs))
+    is_empty = False
+    if len(results) == 0:
+        is_empty = True
+
+    context = {
+        'qs': results,
+        'is_empty': is_empty,
+    }
+
+    return render(request, 'profiles/my_invites.html', context)
