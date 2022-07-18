84390fe52c3783f72c0623b3b3c82840e6e2f7df
jmjry <jakeryan1998@icloud.com>
2022-07-18 17:51:26 +0000

    Created comment model

--- posts/models.py
+++ posts/models.py
@@ -18,5 +18,18 @@ class Post(models.Model):
     def num_likes(self):
         return self.liked.all().count()
 
+    def num_comments(self):
+        return self.comment_set.all().count()
+
     class Meta:
         ordering = ('-created',)
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(max_length=300)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.pk)
