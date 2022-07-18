8f12ee8f95a7d4beb43553c0da5755b4a2d21b0f
jmjry <jakeryan1998@icloud.com>
2022-07-18 17:54:31 +0000

    Created like model

--- posts/models.py
+++ posts/models.py
@@ -34,3 +34,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.pk)
+
+
+
+class Like(models.Model): 
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, max_length=8)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user}-{self.post}-{self.value}"
