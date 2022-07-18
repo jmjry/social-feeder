3bc822be9ba7037d0b0506c1eb9400d1d58ab748
jmjry <jakeryan1998@icloud.com>
2022-07-18 18:31:33 +0000

    Finished profile models

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,39 @@
 from django.db import models
+from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+from django.db.models import Q
 
 # Create your models here.
+
+class ProfileManager(models.Manager):
+
+    def get_all_profiles_to_invite(self, sender):
+        profiles = Profile.objects.all().exclude(user=sender)
+        profile = Profile.objects.get(user=sender)
+        qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+        print(qs)
+        print("#########")
+
+        accepted = set([])
+        for rel in qs:
+            if rel.status == 'accepted':
+                accepted.add(rel.receiver)
+                accepted.add(rel.sender)
+        print(accepted)
+        print("#########")
+
+        available = [profile for profile in profiles if profile not in accepted]
+        print(available)
+        print("#########")
+        return available
+        
+
+    def get_all_profiles(self, me):
+        profiles = Profile.objects.all().exclude(user=me)
+        return profiles
+
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
@@ -18,10 +48,19 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    objects = ProfileManager()
 
+    def __str__(self):
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
+
+
+    def get_absolute_url(self):
+        return reverse("profiles:profile-detail-view", kwargs={"slug": self.slug})
+        
+    
     def get_friends(self):
         return self.friends.all()
-        
+
 
     def get_friends_no(self):
         return self.friends.all().count()
@@ -41,8 +80,8 @@ class Profile(models.Model):
         for item in likes:
             if item.value=='Like':
                 total_liked += 1
-        return 
-        
+        return total_liked
+
 
     def get_likes_recieved_no(self):
         posts = self.posts.all()
@@ -52,11 +91,15 @@ class Profile(models.Model):
         return total_liked
 
 
-    def __str__(self):
-        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
-    
+    __initial_first_name = None
+    __initial_last_name = None
 
-# To check that profiles don't have the same information 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__initial_first_name = self.first_name
+        self.__initial_last_name = self.last_name
+
 
     def save(self, *args, **kwargs):
         ex = False
@@ -73,13 +116,27 @@ class Profile(models.Model):
         self.slug = to_slug
         super().save(*args, **kwargs)
 
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+
+class RelationshipManager(models.Manager):
+    def invatations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
+        return qs
+
 
 class Relationship(models.Model):
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    objects = RelationshipManager()
+
+
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
-        
