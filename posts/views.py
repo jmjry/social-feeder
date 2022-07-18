2392f44899a01e3b8c81605b9548a7e66d30e68e
jmjry <jakeryan1998@icloud.com>
2022-07-18 18:52:53 +0000

    Created post comment create view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_comment_create_and_list_view(request):
+    qs = Post.objects.all()
+
+    context = {
+        'qs': qs,
+    }
+
+    return render(request, 'posts/main.html', context)
