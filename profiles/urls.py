d070a468a7d7ccc167e3bf45123ae08f71c29c3c
jmjry <jakeryan1998@icloud.com>
2022-07-19 19:22:58 +0000

    Created profile detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,7 @@ from .views import (
     invites_received_view, 
     profiles_list_view, 
     invite_profiles_list_view, 
+    ProfileDetailView,
     ProfileListView,
     send_invatation,
     remove_from_friends,
@@ -20,6 +21,7 @@ urlpatterns = [
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
     path('send-invite/', send_invatation, name='send-invite'),
     path('remove-friend/', remove_from_friends, name='remove-friend'),
+    path('<slug>/', ProfileDetailView.as_view(), name='profile-detail-view'),
     path('my-invites/acctept/', accept_invatation, name='accept-invite'),
     path('my-invites/reject/', reject_invatation, name='reject-invite'),
 ]
