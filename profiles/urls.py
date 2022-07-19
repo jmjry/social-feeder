9d8deae641c3c5c3174ed712dc839451a0311a51
jmjry <jakeryan1998@icloud.com>
2022-07-19 06:07:41 +0000

    Created profile list view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-
 from .views import ( 
     my_profile_view, 
     invites_received_view, 
     profiles_list_view, 
-    invite_profiles_list_view, 
+    invite_profiles_list_view,
+    ProfileListView, 
 )
 
 
 app_name = 'profiles'
 
 urlpatterns = [
+    path('', ProfileListView.as_view(), name='all-profiles-view'),
     path('myprofile/', my_profile_view, name='my-profile-view'),
     path('my-invites/', invites_received_view, name='my-invites-view'),
-    path('all-profiles-view', profiles_list_view, name='all-profiles-view'),
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
 ]
