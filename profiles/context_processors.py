9db46830e7c6f76f011cf23cd5af84ac28adc1fc
jmjry <jakeryan1998@icloud.com>
2022-07-20 17:54:00 +0000

    Function displays invitations received

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -7,3 +7,11 @@ def profile_pic(request):
         pic = profile_obj.avatar
         return {'picture':pic}
     return {}
+
+
+def invatations_received_no(request):
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        qs_count = Relationship.objects.invatations_received(profile_obj).count()
+        return {'invites_num':qs_count}
+    return {}
--- socialfeed/settings.py
+++ socialfeed/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'profiles.context_processors.profile_pic',
+                'profiles.context_processors.invatations_received_no',
             ],
         },
     },
